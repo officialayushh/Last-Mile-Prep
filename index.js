ade8abe4573b01f1d3f72b1a078033099bda3ef6
Ayush Saraswat <83123540+officialayushh@users.noreply.github.com>
2022-06-25 11:44:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+// Javascript
 
+// What is the Difference between == vs === ?
+var num1 = 5;
+var num2 = '5';
+console.log(typeof(num1));
+console.log(typeof(num2));
+console.log(num1 == num2 );  //Check Only Value,
+// console.log(num2);
+console.log(num1 === num2 ); // Also check datatype and also value,
+
+
+// Difference between null vs undefined? 
+var iAmUseless = null;  // Given Any Value Null
+console.log(iAmUseless);
+console.log(typeof(iAmUseless));//Bug
+
+var iAmStandBy;  // Not given any value is undefined
+console.log(iAmStandBy);
+console.log(typeof(iAmStandBy));
+
+
+// What is hoisting ?
